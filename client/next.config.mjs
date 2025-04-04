9524b36a2a836b4088cafd6083be21323ae93020
Md Wasim Akram <95247897+wasim0028@users.noreply.github.com>
2025-04-04 20:36:56 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "nextjs-app-0028.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
